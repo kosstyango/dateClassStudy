911c94b22a6439a90c3fd34db94147b1d8393405
Konstantin Barnaev <konstantin.barnaev@gmail.com>
2022-03-31 16:44:18 +0300

    Работа с множествами Set(): создание, объединение, удаление части и распечатка

--- src/Solution.java
+++ src/Solution.java
@@ -1,43 +1,88 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /* 
-Нам повторы не нужны
+Множество всех животных
 */
 
 public class Solution {
     public static void main(String[] args) {
-        removeItemFromMap(createMap());
+        Set<Cat> cats = createCats();
+        System.out.println("Кошки:");
+        printCats(cats);
+        Set<Dog> dogs = createDogs();
+        System.out.println("Собаки:");
+        printDogs(dogs);
+        Set<Object> pets = join(cats, dogs);
+        System.out.println("Все вместе:");
+        printPets(pets);
+
+        removeCats(pets, cats);
+        System.out.println("Без кошек:");
+        printPets(pets);
     }
 
-    public static Map<String, Integer> createMap() {
-        Map<String, Integer> map = new HashMap<>();
-        {
-            map.put("Барнаева", 500);
-            map.put("Захарова", 550);
-            map.put("Петрова", 450);
-            map.put("Сидорова", 300);
-            map.put("Иванова", 200);
-            map.put("Барнаев", 600);
-            map.put("Захаров", 510);
-            map.put("Петровcкий", 480);
-            map.put("Сидоров", 250);
-            map.put("Иванченко", 499);
-        }
-        return map;
+    public static Set<Cat> createCats() { //создаём 4 котов
+        Cat barsik = new Cat("Барсик"); //создание 1 кота
+        Cat murzik = new Cat("Мурзик"); //создание 2 кота
+        Cat chumik = new Cat("Чумик"); //создание 3 кота
+        Cat chuchelo = new Cat("Чучело"); //создание 4 кота
+
+        HashSet<Cat> result = new HashSet<>();
+        result.add(barsik);
+        result.add(murzik);
+        result.add(chumik);
+        result.add(chuchelo);
+        return result;
     }
 
-    public static void removeItemFromMap(Map<String, Integer> map) {
-        Map<String, Integer> copy = new HashMap<>(map);
-        for (Map.Entry<String, Integer> pair : map.entrySet()) {
-            if (pair.getValue() < 500) {
-                System.out.println("Удаляем нищеброда " + pair);//удаляем запись
-                copy.remove(pair.getKey());
-            }
+    public static Set<Dog> createDogs() { //создаём 3 собак
+        Dog kasper = new Dog("Каспер"); //создание 1 собаки
+        Dog julka = new Dog("Жулька"); //создание 2 собаки
+        Dog tuzik = new Dog("Тузик"); //создание 3 собаки
+
+        HashSet<Dog> result = new HashSet<>(); //наполняем собаками массив
+        result.add(kasper);
+        result.add(julka);
+        result.add(tuzik);
+        return result;
+    }
+
+    public static Set<Object> join(Set<Cat> cats, Set<Dog> dogs) {
+            Set pets = new HashSet(cats);
+            pets.addAll(dogs);
+            return pets;
         }
-        System.out.println("Вот что получилось:");
-        for (Map.Entry<String, Integer> pair : copy.entrySet())
-            System.out.println(pair.getKey()+" "+pair.getValue());//печатаем
+
+    public static void removeCats(Set<Object> pets, Set<Cat> cats) {
+        pets.removeAll(cats);
+    }
+    public static void printCats(Set<Cat> cats) {
+        for (Cat cat: cats)
+            System.out.println(cat.name);    }
+
+    public static void printDogs(Set<Dog> dogs) {
+        for (Dog dog: dogs)
+            System.out.println(dog.name);    }
+
+
+    public static void printPets(Set<Object> pets) {
+        for (Object pet: pets)
+            System.out.println(pet);    }
+
+    public static class Cat { //класс Cat
+        String name;    // имя
+
+        public Cat(String name) { //конструктор
+            this.name = name;}
+        }
+
+    public static class Dog { //класс Dog
+        String name;    //имя
+
+        public Dog(String name) {
+            this.name = name;
         }
     }
+}
