cf794fb61e1f91670b3223a8890aa3ba99d82e67
Konstantin Barnaev <konstantin.barnaev@gmail.com>
2022-04-10 21:22:54 +0300

    Вводим 10 строк и считаем кол-во каждой буквы во всех введённых строках, выводим их на экран

--- src/Solution.java
+++ src/Solution.java
@@ -1,88 +1,50 @@
 
-import org.w3c.dom.ls.LSOutput;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
-/*
-Задача по алгоритмам Ӏ Java Syntax: 9 уровень, 11 лекция
+/* 
+Количество букв
 */
 
 public class Solution {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        // Алфавит
+        List<Character> alphabet = Arrays.asList(
+                'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж',
+                'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о',
+                'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц',
+                'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я');
+
+        // Ввод строк
         ArrayList<String> list = new ArrayList<>();
-        String line = reader.readLine();
-        while (!line.isEmpty()) {
-            list.add(line);
-            line = reader.readLine();
+        for (int i = 0; i < 10; i++) {
+            String line = reader.readLine();
+            list.add(line.toLowerCase());
         }
 
-        String[] array = list.toArray(new String[0]);
-        sort(array);
-        for (String x : array) {
-            System.out.println(x);
+        //HashMap<Character, Integer> map = new HashMap<>();
+
+        for (int a = 0; a < 33; a++) {
+            //map.put(alphabet.get(a), counter(alphabet.get(a), list));
+            System.out.println(alphabet.get(a)+" "+ counter(alphabet.get(a), list));
         }
+
     }
 
-    public static void sort(String[] array) {
-    ArrayList <Integer> digitList = new ArrayList<>();
-    ArrayList <String> stringList = new ArrayList<>();
-        for(String s : array){
-            if(isNumber(s)? digitList.add(Integer.parseInt(s)) : stringList.add(s));
-        }
-        digitList.sort(Comparator.reverseOrder());
-        System.out.println("Отсортированы цифры "+digitList.toString());
-        stringList.sort(Comparator.naturalOrder());
-        System.out.println("Отсортированы строки "+stringList.toString());
-        //if (isGreaterThan("a", "b"));
-
-        for(int i=0; i<digitList.size(); i++)
+    public static Integer counter(Character letter, ArrayList list) {
+        int count = 0;
+        for(int i=0; i< list.size(); i++)
         {
-            System.out.println("Записываем в итоговый массив число "+digitList.get(i) + " на место "+i);
-            array[i] = String.valueOf(digitList.get(i));
-        }
-
-        for(int j=0; j<stringList.size(); j++)
-        {
-            System.out.println("Записываем в итоговый массив число "+stringList.get(j) + " на место "+j);
-            array[digitList.size()+j] = stringList.get(j);
-        }
-    }
-    // Метод для сравнения строк: 'а' больше чем 'b'
-    public static boolean isGreaterThan(String a, String b) {
-        return a.compareTo(b) > 0;
-    }
-
-
-    // Переданная строка - это число?
-    public static boolean isNumber(String text) {
-        if (text.length() == 0) {
-            return false;
-        }
-
-        char[] chars = text.toCharArray();
-        for (int i = 0; i < chars.length; i++) {
-            char character = chars[i];
-
-            // есть '-' внутри строки
-            if (i != 0 && character == '-') {
-                return false;
-            }
-
-            // не цифра и не начинается с '-'
-            if (!Character.isDigit(character) && character != '-') {
-                return false;
-            }
-
-            // одиночный '-'
-            if (chars.length == 1 && character == '-') {
-                return false;
+            for(int j=0; j<list.get(i).toString().length(); j++) {
+                if (list.get(i).toString().charAt(j) == letter) count++;
+                //System.out.println("Проверяем элемент " + list.get(i).toString().charAt(j) + ". Буква " + letter + " попалась " + count + " раз");
             }
         }
-
-        return true;
-    }
+    return count;}
 }
